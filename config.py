806682fd310bd29b65700bfa0dc20978513bc82c
Mahabul29 <Mahabulbhai288550@gmail.com>
2024-08-12 14:12:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "5733685945"))
+API_HASH = environ.get("API_HASH", "6d860c95ed52acc0641537796a40215d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7490064385:AAGfe_KQG7NkyrVKRCuA5ROvrX97bejyVV8")
+ADMIN = int(environ.get("ADMIN", "5733685945"))          
+CAPTION = environ.get("CAPTION", "@Hindi_Dub_Animes_Official")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
